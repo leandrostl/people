d712a1c8aceb5301ddfbba0836f01fdc3f5c380a
Leandro Lima <leandro.stlima@gmail.com>
2019-03-04 19:36:42 -0300

    Usa Dto para responser na api, melhora o desacoplamento.

---
+++ src/main/java/application/control/ResultDto.java
@@ -0,0 +1,8 @@
+package application.control;
+
+import lombok.Data;
+
+public @Data class ResultDto {
+
+	private final String result;
+}
--- src/main/java/application/domain/Person.java
+++ src/main/java/application/domain/Person.java
@@ -5,11 +5,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
-public @Data @Builder class Person {
+public @Data @Builder @NoArgsConstructor @AllArgsConstructor class Person {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
