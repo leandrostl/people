22917a4a17bd2c4f1beaabce31c4737bc2c73365
Leandro Lima <leandro.stlima@gmail.com>
2019-03-04 19:38:01 -0300

    Cria testes de integração usando banco H2.

--- src/main/java/application/control/PeopleController.java
+++ src/main/java/application/control/PeopleController.java
@@ -5,16 +5,17 @@ import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import application.Messages;
 import application.domain.Person;
 import application.service.PeopleService;
 
@@ -29,25 +30,25 @@ public class PeopleController {
 	private ModelMapper modelMapper;
 
 	@GetMapping(path = "/pessoas", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<PersonDto>> getPeople() {
-		return new ResponseEntity<>(service.getPeople().stream().map(person -> modelMapper.map(person, PersonDto.class))
-				.collect(Collectors.toList()), HttpStatus.OK);
+	public @ResponseBody List<PersonDto> getPeople() {
+		return service.getPeople().stream().map(person -> modelMapper.map(person, PersonDto.class))
+				.collect(Collectors.toList());
 	}
 
 	@GetMapping(path = "/pessoa/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<PersonDto> getPerson(@PathVariable final Long id) {
-		return new ResponseEntity<>(modelMapper.map(service.getPerson(id), PersonDto.class), HttpStatus.OK);
+	public @ResponseBody PersonDto getPerson(@PathVariable final Long id) {
+		return modelMapper.map(service.getPerson(id), PersonDto.class);
 	}
 
-	@PostMapping(path = "/pessoa/save", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<PersonDto>> setPerson(@RequestBody final PersonDto person) {
+	@PostMapping(path = "/pessoa/save", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody ResultDto setPerson(@RequestBody final PersonDto person) {
 		service.setPerson(modelMapper.map(person, Person.class));
-		return new ResponseEntity<>(HttpStatus.OK);
+		return new ResultDto(Messages.getString("success"));
 	}
 
-	@GetMapping(path = "/pessoa/remove/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<PersonDto>> removePerson(@PathVariable final Long id) {
+	@DeleteMapping(path = "/pessoa/remove/{id}")
+	public @ResponseBody ResultDto removePerson(@PathVariable final Long id) {
 		service.removePerson(id);
-		return new ResponseEntity<>(HttpStatus.OK);
+		return new ResultDto(Messages.getString("success"));
 	}
 }
--- src/test/java/application/control/PeopleControllerTest.java
+++ src/test/java/application/control/PeopleControllerTest.java
@@ -1,18 +1,101 @@
 package application.control;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import application.Messages;
+import application.domain.Person;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 public class PeopleControllerTest {
 
 	@Autowired
 	private MockMvc mvc;
 
+	@Test
+	public void testGetPeople() throws Exception {
+		mvc.perform(get("/rest/pessoas")).andExpect(status().isOk()).andExpect(jsonPath("$").isEmpty());
+	}
+
+	@Test
+	public void testSetPerson() throws Exception {
+		final String person = createPerson();
+		mvc.perform(postPerson(person)).andExpect(status().isOk()).andDo(result -> mvc.perform(get("/rest/pessoas"))
+				.andExpect(status().isOk()).andExpect(jsonPath("$[0].name").value(equalTo("P1"))));
+	}
+
+	@Test
+	public void testGetPerson() throws Exception {
+		final Long id = postPersonAndGetId();
+		mvc.perform(get("/rest/pessoa/" + id)).andExpect(status().isOk())
+				.andExpect(jsonPath("$.name").value(equalTo("P1")));
+	}
+
+	@Test
+	public void testGetPerson_nonExistent() throws Exception {
+		final String message = mvc.perform(get("/rest/pessoa/1")).andExpect(status().isBadRequest()).andReturn()
+				.getResolvedException().getMessage();
+		assertThat(message, equalTo(Messages.getString("error_not_found", 1)));
+	}
+
+	@Test
+	public void testRemovePerson() throws Exception {
+		final Long id = postPersonAndGetId();
+		mvc.perform(delete("/rest/pessoa/remove/" + id)).andExpect(status().isOk());
+		mvc.perform(get("/rest/pessoas")).andExpect(status().isOk()).andExpect(jsonPath("$").isEmpty());
+	}
+
+	@Test
+	public void testRemovePerson_nonExistent() throws Exception {
+		final String message = mvc.perform(delete("/rest/pessoa/remove/1")).andExpect(status().isBadRequest())
+				.andReturn().getResolvedException().getMessage();
+		assertThat(message, equalTo(Messages.getString("error_not_found", 1)));
+	}
+
+	private Long postPersonAndGetId() throws JsonProcessingException, Exception, IOException, JsonParseException,
+			JsonMappingException, UnsupportedEncodingException {
+
+		mvc.perform(postPerson(createPerson()));
+		final String result = mvc.perform(get("/rest/pessoas")).andDo(print()).andReturn().getResponse()
+				.getContentAsString();
+		return new ObjectMapper().readValue(result, Person[].class)[0].getId();
+	}
+
+	private MockHttpServletRequestBuilder postPerson(final String person) {
+		return post("/rest/pessoa/save").contentType(MediaType.APPLICATION_JSON_VALUE).content(person);
+	}
+
+	private String createPerson() throws JsonProcessingException {
+		return new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(
+				Person.builder().id(0L).name("P1").city("Belo Horizonte").state("Minas Gerais").build());
+	}
 }
